5697e4fc36e8261434c4bf9e5927091714404d02
ilin-t <i.tolovski@gmail.com>
2023-08-03 17:20:09 +0200

    Test slurm script

---
+++ setup_node.sh
@@ -0,0 +1,24 @@
+# Install
+python3 -m venv venv
+source venv/bin/activate
+
+# Install the requirements for Stork
+python3 -m pip install -r requirements.txt
+
+# Download Python repositories from Github. Downloads all repositories from 2018 onwards.
+#python3 src/log_modules/get_repos.py
+#python3 src/log_modules/parallel_download.py
+#python3 src/log_modules/parse_repos.py -t 12
+#python3 src/log_modules/analyze_repos.py
+#python3 src/log_modules/process_requirements.py
+#python3 src/log_modules/plot_libraries.py
+#python3 src/log_modules/parse_requirements.py -t 12
+python3 src/log_modules/plot_libraries.py --path=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/node_test/
+
+
+
+#sudo -HE /home/ilint/HPI/repos/stork/venv/bin/python src/log_modules/get_repos.py
+#sudo -HE /home/ilint/HPI/repos/stork/venv/bin/python src/log_modules/analyze_repos.py
+#sudo -HE /home/ilint/HPI/repos/stork/venv/bin/python src/log_modules/process_requirements.py
+
+
