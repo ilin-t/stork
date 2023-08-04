c88c2dd0d8a8ee8e12700f1357f49f5c78944c91
ilin-t <i.tolovski@gmail.com>
2023-08-04 13:18:43 +0200

    Cluster configuration

--- setup_node.sh
+++ setup_node.sh
@@ -6,6 +6,11 @@ source venv/bin/activate
 # Install the requirements for Stork
 python3 -m pip install -r requirements.txt
 
-python3 src/log_modules/parse_repos.py --threads=12 --repos=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/repositories-test/ --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/ --outputs=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/outputs/
-python3 src/log_modules/parse_requirements.py --threads=12 --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/
-python3 src/log_modules/plot_libraries.py --path=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/
+#python3 src/log_modules/parse_repos.py --threads=12 --repos=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/repositories-test/ --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/ --outputs=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/outputs/
+#python3 src/log_modules/parse_requirements.py --threads=12 --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/
+#python3 src/log_modules/plot_libraries.py --path=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-2days/packages/
+
+
+python3 src/log_modules/parse_repos.py --threads=12 --repos=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-download/repositories-test/ --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-download/packages/ --outputs=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-download/outputs/
+python3 src/log_modules/parse_requirements.py --threads=12 --packages=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-download/packages/
+python3 src/log_modules/plot_libraries.py --path=/hpi/fs00/share/fg-rabl/ilin.tolovski/stork-zip-download/packages/
