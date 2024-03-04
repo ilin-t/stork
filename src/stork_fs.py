fa102f654f6a5844b448256a2cb7da9cd3736dcb
ilin-t <i.tolovski@gmail.com>
2024-03-04 20:02:09 +0100

    Added dev branch

--- src/stork_fs.py
+++ src/stork_fs.py
@@ -20,19 +20,19 @@ from src.log_modules.log_results import createLogger, createLoggerPlain
 
 class Stork:
 
-    def __init__(self, config_path, logger, connector):
-
-        if "s3" in connector:
-            self.connector = S3Connector()
-        elif "postgres" in connector:
-            self.connector = PsqlConnector(config_path)
-            self.connector.set_logger(logger)
-        else:
-            self.connector = None
+    def __init__(self, logger):
+        #
+        # if "s3" in connector:
+        #     self.connector = S3Connector()
+        # elif "postgres" in connector:
+        #     # self.connector = PsqlConnector(config_path)
+        #     self.connector.set_logger(logger)
+        # else:
+        self.connector = None
         self.assignVisitor = AssignVisitor()
         self.pipeline = ""
         self.logger = logger
-        self.config_path = config_path
+        self.config_path = None
         # self.access_key, self.secret_access_key = self.parseConfig(config_path=self.config_path)
         self.config = None
         self.assignments = {}
@@ -53,7 +53,8 @@ class Stork:
 
         try:
             script_name, file_ending = pipeline.split(".")
-            self.assignVisitor.transformScript(pipeline, script_name + "_new." + file_ending)
+            self.assignVisitor.transformScript(script=pipeline, new_script=script_name + "_new." + file_ending,
+                                               datasets_urls=self.datasets_urls)
         except len(pipeline.split(".")) > 2:
             print("Pipeline name has wrong formatting.")
 
@@ -98,19 +99,17 @@ class Stork:
                 schema_gen_start = time.time_ns()
                 # schema_string = self.connector.generate_schema(dataset_df)
                 schema_gen_end = time.time_ns() - schema_gen_start
-                self.logger.info(f"Schema generation for {dataset_name}: {schema_gen_end/1000000} ms")
-                self.schema_generation_times[dataset_name]=(schema_gen_end/1000000)
+                self.logger.info(f"Schema generation for {dataset_name}: {schema_gen_end / 1000000} ms")
+                self.schema_generation_times[dataset_name] = (schema_gen_end / 1000000)
                 print(dataset_name)
                 # if self.connector.create_table(table_name=f"{schema_name}.{dataset_name}", schema_order=schema_string):
 
                 insert_start = time.time_ns()
                 shutil.copy(abs_path_dataset, "/mnt/fs00/ilin.tolovski/{dataset}_{pipeline}.csv")
                 insert_end = time.time_ns() - insert_start
-                self.table_insertion_times[dataset_name]=(insert_end/1000000)
-                self.logger.info(f"Insertion time for {dataset_name}: {insert_end/1000000}ms")
+                self.table_insertion_times[dataset_name] = (insert_end / 1000000)
+                self.logger.info(f"Insertion time for {dataset_name}: {insert_end / 1000000}ms")
                 # self.logger.get_one(f"{schema_name}.{dataset_name}")
-
-
 
     def parseConfig(self, config_path):
         config = ConfigParser()
@@ -118,6 +117,7 @@ class Stork:
         credentials = config['credentials']
 
         return credentials["aws_access_key_id"], credentials["aws_secret_access_key"]
+
 
 def extract_files():
     root_path = "/home/ilint/HPI/Stork/stork-dolly-example/pipelines/"
@@ -140,30 +140,32 @@ def extract_files():
                 f.write("\n")
         f.close()
 
-def run_stork(args):
 
+def run_stork(args):
     # pipelines = get_repository_list(f"{args.repositories}/{args.mode}_full_paths.txt")
     pipelines = [f.path for f in os.scandir(args.repositories) if f.is_file()]
     print(pipelines)
     output_logger = createLoggerPlain(filename=f"{args.outputs}/paper_example_times.log",
-                                 project_name=f"paper_example_outputs",
-                                 level=logging.INFO)
-    stats={}
+                                      project_name=f"paper_example_outputs",
+                                      level=logging.INFO)
+    stats = {}
     for pipeline in pipelines:
         pipeline_name = getDatasetName(pipeline.strip())
-        logger = createLogger(filename=f"{args.individual_logs}/{pipeline_name}.log", project_name=f"{pipeline_name}_project",
+        logger = createLogger(filename=f"{args.individual_logs}/{pipeline_name}.log",
+                              project_name=f"{pipeline_name}_project",
                               level=logging.INFO)
-        stork = Stork(logger = logger, config_path="db_conn/config_db.ini", connector="postgres")
+        stork = Stork(logger=logger)
 
-        stork.setup(pipeline = pipeline.strip(), new_pipeline=f"new_{pipeline}.py")
+        stork.setup(pipeline=pipeline.strip(), new_pipeline=f"new_{pipeline}.py")
 
-        stats[pipeline]= {"translation_time": stork.translation_times,
-                          "datasets": {"schema_gen": stork.schema_generation_times},
-                                        "table_creation": stork.table_creation_times,
-                                        "table_insertion": stork.table_insertion_times,
-                                        "data_sizes": stork.dataframe_sizes
-                          }
+        stats[pipeline] = {"translation_time": stork.translation_times,
+                           "datasets": {"schema_gen": stork.schema_generation_times},
+                           "table_creation": stork.table_creation_times,
+                           "table_insertion": stork.table_insertion_times,
+                           "data_sizes": stork.dataframe_sizes
+                           }
         output_logger.info(f"{pipeline.split('/')[-1].strip()}: {stats[pipeline]}")
+
 
 def main(args):
     os.makedirs(args.repositories, exist_ok=True)
