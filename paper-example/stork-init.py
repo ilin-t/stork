89cef43edaf7f7da26082fc3d8a5daf6bb5f2a42
ilin-t <i.tolovski@gmail.com>
2024-03-04 20:12:57 +0100

    Added example files.

---
+++ paper-example/stork-init.py
@@ -0,0 +1,19 @@
+import logging
+
+from src.log_modules.log_results import createLogger
+from src.stork_fs import Stork
+
+
+def main():
+    logger = createLogger(filename=f"logs/example.log",
+                          project_name=f"pipelines/example.py",
+                          level=logging.INFO)
+
+    stork = Stork(logger=logger)
+    logger.info("Stork has been initialized.")
+    stork.setPipeline(pipeline="pipelines/example.py")
+    logger.info(f"{stork.pipeline} is set.")
+
+
+if __name__ == '__main__':
+    main()
