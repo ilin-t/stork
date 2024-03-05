877560edc5eb13f1ddfa03784b82bc9d25362028
ilin-t <i.tolovski@gmail.com>
2024-03-05 22:17:09 +0100

    Added evaluation files.

--- generate-plots/generate-plots.sh
+++ generate-plots/generate-plots.sh
@@ -2,12 +2,5 @@ export PYTHONPATH=${PYTHONPATH}:/usr/bin/python3
 
 mkdir plots
 cd python_files
-#python3 plot_distributions.py
-#python3 plot_library_distribution.py
-#python3 plot_library_tail.py
-#python3 plot_llm_results.py
-#python3 plot_pipeline_dist.py
-##python3 plot_ratios.py
-
 
 for f in *.py; do python3 "$f"; done
